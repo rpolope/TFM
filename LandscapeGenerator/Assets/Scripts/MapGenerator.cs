443247fa80181c7da495a208d8fd58fc986aae05
Rafa <rafikax12@gmail.com>
2024-06-06 16:56:47 +0200

    Cambios en MapGenerator.cs: Eliminación del job de generación del colorMap y del método GenerateColorMap

--- LandscapeGenerator/Assets/Scripts/MapGenerator.cs
+++ LandscapeGenerator/Assets/Scripts/MapGenerator.cs
@@ -51,79 +51,13 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-    [BurstCompile]
-    private struct GenerateColorMapJob : IJobParallelFor
-    {
-        [NativeDisableParallelForRestriction] 
-        public NativeArray<float> HeightMap;
-        [NativeDisableParallelForRestriction] 
-        public NativeArray<Color> ColorMap;
-        [NativeDisableParallelForRestriction] 
-        public NativeArray<Color> BiomeMoistureColorRange;
-        public float LatitudeHeat;
-        public float ChunkMoisture;
-
-        public Color BiomeColor;
-        
-        public void Execute(int threadIndex)
-        {
-            var height = HeightMap[threadIndex];
-            var heightTempDecr = height * 0.6f;
-            var heat = Mathf.Clamp01(LatitudeHeat - heightTempDecr);
-            ColorMap[threadIndex] = BiomeMoistureColorRange[Mathf.RoundToInt(LatitudeHeat * 127)] * height;
-        }
-        
-        private void GenerateMoistureBasedColor(int threadIndex)
-        {
-            var color = Color.Lerp(Color.red, Color.blue, ChunkMoisture);
-            ColorMap[threadIndex] = color * HeightMap[threadIndex];
-        }
-        
-        private void GenerateLatitudeHeatBasedColor(int threadIndex)
-        {
-            ColorMap[threadIndex] = Color.Lerp(Color.blue, Color.red, LatitudeHeat);
-        }
-
-        private void GenerateHeightBasedColor(int threadIndex)
-        {
-            ColorMap[threadIndex] = Color.Lerp(Color.black, Color.white, HeightMap[threadIndex]);
-        }
-    }
-
     public static MapData GenerateMapData(int resolution, NoiseParameters parameters, Biome biome, float2 centre = new float2()) {
 
         BiomesManager.Initialize();
         
         float[] noiseMap = GenerateNoiseMap(resolution, centre, parameters);
         
-        // Color[] colorMap = GenerateColorMap(noiseMap, biome);
-	
         return new MapData (noiseMap);
-    }
-
-    private static Color[] GenerateColorMap(float[] noiseMap, Biome biome)
-    {
-        NativeArray<float> heightMap = new NativeArray<float>(noiseMap, Allocator.Persistent);
-        NativeArray<Color> colorMap = new NativeArray<Color>(noiseMap.Length, Allocator.Persistent);
-        NativeArray<Color> colorRange = new NativeArray<Color>(biome.ColorGradient, Allocator.Persistent);
-        
-
-        GenerateColorMapJob colorMapJob = new GenerateColorMapJob()
-        {
-            HeightMap = heightMap,
-            ColorMap = colorMap,
-            BiomeMoistureColorRange = colorRange,
-            LatitudeHeat = biome.Heat,
-            ChunkMoisture = biome.Moisture,
-        };
-        colorMapJob.Schedule(colorMap.Length, 64).Complete();
-
-        var colorMapArray = colorMap.ToArray();
-        heightMap.Dispose();
-        colorMap.Dispose();
-        colorRange.Dispose();
-        
-        return colorMapArray;
     }
 
     public static float[] GenerateNoiseMap(int resolution, float2 centre, NoiseParameters parameters)
