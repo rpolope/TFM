8895f0460cda2f89740d2835fd04ee4c3f51d52e
Rafa <rafikax12@gmail.com>
2024-06-07 16:34:53 +0200

    Se mezclan los colores en los límites, NO TIENEN LÍMITES ESTRICTOS!
    Cambios en Terrain.shader: Se ha agregado el array de blends, la constante epsilon, un atributo de tipo texture2d y textureScale y se ha cambiado la manera de asignar valor al albedo usando el array de blends.
    
    Cambios en ChunkMaterial.mat: Se le ha agregado una textura (la del traibuto del shader)
    
    Cambios en TextureData.cs: se han añadido dos constantes y se ha pasado el array de blends al material. También se han pasado las asignaciones de las variables del shader al método ApplyToMaterial.

--- LandscapeGenerator/Assets/Scripts/Data/TextureData.cs
+++ LandscapeGenerator/Assets/Scripts/Data/TextureData.cs
@@ -4,9 +4,14 @@ using System.Collections;
 [CreateAssetMenu()]
 public class TextureData : UpdatableData {
     
+    private const int TextureSize = 512;
+    private const TextureFormat TextureFormat = UnityEngine.TextureFormat.RGB565;
+    
     public Color[] baseColours;
     [Range(0,1)]
     public float[] baseStartHeights;
+    [Range(0,1)]
+    public float[] baseBlends;
     
     private float _savedMinHeight;
     private float _savedMaxHeight;
@@ -16,9 +21,15 @@ public class TextureData : UpdatableData {
     private static readonly int BaseColourCount = Shader.PropertyToID("baseColourCount");
     private static readonly int BaseColours = Shader.PropertyToID("baseColours");
     private static readonly int BaseStartHeights = Shader.PropertyToID("baseStartHeights");
+    private static readonly int BaseBlends = Shader.PropertyToID("baseBlends");
 
     public void ApplyToMaterial(Material material)
     {
+        material.SetInt (BaseColourCount, baseColours.Length);
+        material.SetColorArray (BaseColours, baseColours);
+        material.SetFloatArray (BaseStartHeights, baseStartHeights);
+        material.SetFloatArray (BaseBlends, baseBlends);
+        
         UpdateMeshHeights(material, _savedMinHeight, _savedMaxHeight);
     }
 
@@ -26,10 +37,6 @@ public class TextureData : UpdatableData {
     {
         _savedMinHeight = minHeight;
         _savedMaxHeight = maxHeight;
-        
-        material.SetInt (BaseColourCount, baseColours.Length);
-        material.SetColorArray (BaseColours, baseColours);
-        material.SetFloatArray (BaseStartHeights, baseStartHeights);
         
         material.SetFloat(MinHeight, minHeight);
         material.SetFloat(MaxHeight, maxHeight);
