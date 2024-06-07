81bf7b5383f2c931013e3a7cb234f0071e44a35b
Rafa <rafikax12@gmail.com>
2024-06-07 17:59:32 +0200

    FINALMENTE SE MEZCLAN LAS TEXTURAS EN BASE A SU ALTURA!!
    Cambios en TextureData.cs: Se ha creado un nuevo tipo de dato: Layer, que contiene la información de color y textura de cada altura de terreno, junto a su nivel de intensidad y su ancho de mezclado. Se han añadido nuevos atributos al shader como el array de texturas, el color strength y se han renombrado algunas variables.
    
    Cambios en Terrain.shader: Se ha creado el método triplanar que samplea el valor de la textura que se le pasa de aucerdo al valor de la normal en el fragmento que se está procesando y se han renombrado algunas variables.

--- LandscapeGenerator/Assets/Scripts/Data/TextureData.cs
+++ LandscapeGenerator/Assets/Scripts/Data/TextureData.cs
@@ -1,44 +1,69 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 [CreateAssetMenu()]
 public class TextureData : UpdatableData {
     
     private const int TextureSize = 512;
     private const TextureFormat TextureFormat = UnityEngine.TextureFormat.RGB565;
-    
-    public Color[] baseColours;
-    [Range(0,1)]
-    public float[] baseStartHeights;
-    [Range(0,1)]
-    public float[] baseBlends;
-    
-    private float _savedMinHeight;
-    private float _savedMaxHeight;
-    
+
+    public Layer[] layers;
+
+    float _savedMinHeight;
+    float _savedMaxHeight;
+    private static readonly int LayerCount = Shader.PropertyToID("layerCount");
+    private static readonly int BaseColors = Shader.PropertyToID("baseColors");
+    private static readonly int BaseBlends = Shader.PropertyToID("baseBlends");
+    private static readonly int BaseStartHeights = Shader.PropertyToID("baseStartHeights");
+    private static readonly int BaseColorStrength = Shader.PropertyToID("baseColorStrength");
+    private static readonly int BaseTextureScales = Shader.PropertyToID("baseTextureScales");
+    private static readonly int BaseTextures = Shader.PropertyToID("baseTextures");
     private static readonly int MinHeight = Shader.PropertyToID("minHeight");
     private static readonly int MaxHeight = Shader.PropertyToID("maxHeight");
-    private static readonly int BaseColourCount = Shader.PropertyToID("baseColourCount");
-    private static readonly int BaseColours = Shader.PropertyToID("baseColours");
-    private static readonly int BaseStartHeights = Shader.PropertyToID("baseStartHeights");
-    private static readonly int BaseBlends = Shader.PropertyToID("baseBlends");
 
-    public void ApplyToMaterial(Material material)
-    {
-        material.SetInt (BaseColourCount, baseColours.Length);
-        material.SetColorArray (BaseColours, baseColours);
-        material.SetFloatArray (BaseStartHeights, baseStartHeights);
-        material.SetFloatArray (BaseBlends, baseBlends);
-        
-        UpdateMeshHeights(material, _savedMinHeight, _savedMaxHeight);
+    public void ApplyToMaterial(Material material) {
+		
+        material.SetInt (LayerCount, layers.Length);
+        material.SetColorArray (BaseColors, layers.Select(x => x.tint).ToArray());
+        material.SetFloatArray (BaseBlends, layers.Select(x => x.blendStrength).ToArray());
+        material.SetFloatArray (BaseStartHeights, layers.Select(x => x.startHeight).ToArray());
+        material.SetFloatArray (BaseColorStrength, layers.Select(x => x.tintStrength).ToArray());
+        material.SetFloatArray (BaseTextureScales, layers.Select(x => x.textureScale).ToArray());
+        material.SetTexture (BaseTextures, GenerateTextureArray (layers.Select (x => x.texture).ToArray ()));
+
+        UpdateMeshHeights (material, _savedMinHeight, _savedMaxHeight);
     }
 
-    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
-    {
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight) {
         _savedMinHeight = minHeight;
         _savedMaxHeight = maxHeight;
-        
-        material.SetFloat(MinHeight, minHeight);
-        material.SetFloat(MaxHeight, maxHeight);
+
+        material.SetFloat (MinHeight, minHeight);
+        material.SetFloat (MaxHeight, maxHeight);
     }
+
+    Texture2DArray GenerateTextureArray(Texture2D[] textures) {
+        Texture2DArray textureArray = new Texture2DArray (TextureSize, TextureSize, textures.Length, TextureFormat, true);
+        for (int i = 0; i < textures.Length; i++) {
+            textureArray.SetPixels (textures [i].GetPixels (), i);
+        }
+        textureArray.Apply ();
+        return textureArray;
+    }
+
+    [System.Serializable]
+    public class Layer {
+        public Texture2D texture;
+        public Color tint;
+        [Range(0,1)]
+        public float tintStrength;
+        [Range(0,1)]
+        public float startHeight;
+        [Range(0,1)]
+        public float blendStrength;
+        public float textureScale;
+    }
+		
+	 
 }
